2ece232a8d6bed1b3b32c0c2a6446cde396f33d2
Diego <diegomez27@outlook.com>
2025-12-26 09:09:35 -0700

    ip de la casa en archivos env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.1.74:3001',
+  //apiUrl: 'http://192.168.1.74:3001', // oficina
+  apiUrl: 'http://192.168.100.23:3001', // casa
+  // apiUrl: 'http://172.20.10.3:3001', // ip del hotspot de la pi
   apiTimeout: 30000,
   cacheTimeout: 300000,
   appVersion: '1.0.0'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'http://192.168.137.128:3001',
-   apiUrl: 'http://192.168.1.74:3001',
-   //apiUrl: 'http://192.168.100.23:3001',
+  //apiUrl: 'http://192.168.1.74:3001', // oficina
+  apiUrl: 'http://192.168.100.23:3001', // casa
+  //apiUrl: 'http://172.20.10.3:3001', // ip del hotspot de la pi
   apiTimeout: 30000,
   cacheTimeout: 300000,
   appVersion: '2.0.0'
